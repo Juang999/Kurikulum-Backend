245ad4e10e7d00771bb09e6a85d8d6e957ad45b0
Juang999 <juggernaut.9909@g,ail.com>
2020-09-11 12:54:27 +0700

    tugas5.2

--- sprint-1/topik-5/tugas2/tugas-1.php
+++ sprint-1/topik-5/tugas2/tugas-1.php
@@ -1,22 +1,7 @@
 <?php
-//LINGKARAN
-$lingkaran = function($r):int {
-    return 22/7*($r*$r);
+$calcRectangleArea = function($width, $length){
+return $width*$length;
 };
-echo $lingkaran(6);
-echo "\n";
-
-//SEGITIGA
-$segitiga = function($a, $t):int {
-    return ($a*$t)/2;
-};
-echo $segitiga(10, 5);
-echo "\n";
-
-//TRAPESIUM
-$trapesium = function($alas1, $alas2, $tinggi){
-    return (($alas1*$alas2)*$tinggi)/2;
-};
-echo $trapesium(18, 6, 10);
+echo "calcRectangleArea: ".$calcRectangleArea(5, 10);
 echo "\n";
 ?>
--- sprint-1/topik-5/tugas2/tugas-2.php
+++ sprint-1/topik-5/tugas2/tugas-2.php
@@ -1,7 +1,22 @@
 <?php
-$calcRectangleArea = function($width, $length){
-return $width*$length;
+//LINGKARAN
+$lingkaran = function($r):int {
+    return 22/7*($r*$r);
 };
-echo $calcRectangleArea(5, 10);
+echo "lingkaran: ".$lingkaran(6);
+echo "\n";
+
+//SEGITIGA
+$segitiga = function($a, $t):int {
+    return ($a*$t)/2;
+};
+echo "segitiga: ".$segitiga(10, 5);
+echo "\n";
+
+//TRAPESIUM
+$trapesium = function($alas1, $alas2, $tinggi){
+    return (($alas1*$alas2)*$tinggi)/2;
+};
+echo "trapesium: ".$trapesium(18, 6, 10);
 echo "\n";
 ?>
